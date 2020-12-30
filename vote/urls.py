1c3288ae8359d96d4224b27b9b99b781c5e5523b
Danil Usmanov <usmanovdanilr@gmail.com>
2020-12-30 17:10:04 +0300

    + swagger and user logic

--- vote/urls.py
+++ vote/urls.py
@@ -1,15 +1,18 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from vote.swagger import schema_view
-from vote.views import answer, vote, question, user
+from vote.views import answer, question, user, vote
 
 router = DefaultRouter()
 # router.register("vote", vote.VoteViewSet, basename="vote")
-router.register("question", question.QuestionViewSet, basename="question")
-router.register("answer", answer.AnswerViewSet, basename="answer")
+# router.register("question", question.QuestionViewSet, basename="question")
+# router.register("answer", answer.AnswerViewSet, basename="answer")
 
 router.register("get_active_votes", vote.VoteViewSet, basename="get_active_votes")
-router.register("get_user_id", user.UserAuthorizationViewSet,basename="get_user_id")
+router.register("get_user_id", user.UserAuthorizationViewSet, basename="get_user_id")
+router.register("send_answers", user.UserSubmissionViewSet, basename="send_answers")
+router.register("get_user_votes", user.UserVotesViewSet, basename="get_user_votes")
 
 api_urls = [*router.urls]
 
