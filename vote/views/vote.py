c320307c5f06ef9c1b12dd8ec70666eab21ec566
Danil Usmanov <usmanovdanilr@gmail.com>
2021-01-11 13:01:09 +0300

    + pagination

--- vote/views/vote.py
+++ vote/views/vote.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import pytz
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 
 from vote import models
 from vote.serializers.vote import VoteSerializer
@@ -13,6 +14,7 @@ class VoteViewSet(ReadOnlyModelViewSet):
     """Returns list of active votes"""
 
     serializer_class = VoteSerializer
+    pagination_class = LimitOffsetPagination
     queryset = models.Vote.objects.filter(
         start_date__lte=datetime.now(tz=pytz.timezone("Europe/Moscow")),
         end_date__gte=datetime.now(tz=pytz.timezone("Europe/Moscow")),
